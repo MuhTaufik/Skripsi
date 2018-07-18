e71d372a75d7036a77121afc5f232882c393ec35
MuhTaufik <taufikm91@gmail.com>
2018-07-18 20:42:08 +0700

    doc program
    program fix

--- doc/DokumenSkripsi/Program/ins.js
+++ doc/DokumenSkripsi/Program/ins.js
@@ -1,38 +1,75 @@
-var Viz = require("viz.js");
-var http = require('http');
-var axios = require('axios');
-//axios library buat http req, karena untuk ngakses data raw di github perlu req data melalui http
+var Viz = require('viz.js'); //visualisasi dalam bentuk grafik
+var http = require('http');  //menjalankan server di web 
+var axios = require('axios');//library untuk mengakses data raw di github perlu request data melalui http.
+
+var engine, pilihan, resultGraph;
 
 http.createServer(function (req, res) {
-    axios.get("http://raw.githubusercontent.com/ftisunpar/data/master/prasyarat.json")
-        .then(function (data) {
-            var graphDot = [
-                "digraph G {",
-                rankSep(data.data),
-                nodesMatkul(data.data),
-                edgesMatkul(data.data),
-                "}",
-              ].join("\n");
-              var resultGraph = Viz(graphDot, { format: "svg"});
-              res.write(resultGraph);
-              res.end();
-        })
+  var params = req.url.substring(req.url.lastIndexOf('?') + 1).split('&');
+  for (var i = 0; i < params.length; i++) {
+    var paramKeyValPair = params[i].split('=');
+    if (paramKeyValPair[0] === 'engine') {
+      engine = paramKeyValPair[1];
+    } else if (paramKeyValPair[0] === 'pilihan') {
+      pilihan = paramKeyValPair[1];
+    } else {
+      engine = 'dot';
+      pilihan = 'false';
+    }
+  }
+  console.log(engine, pilihan)
+  axios.get("http://raw.githubusercontent.com/ftisunpar/data/master/prasyarat.json")
+    .then(data => {
+      var graphDot = [
+        "digraph G {",
+        rankSep(data.data),
+        nodesMatkul(data.data),
+        edgesMatkul(data.data),
+        "}",
+      ].join("\n");
+      switch (engine) {
+        case "circo":
+          resultGraph = Viz(graphDot, { format: "svg", engine: "circo" });
+          break;
+        case "fdp":
+          resultGraph = Viz(graphDot, { format: "svg", engine: "fdp" });
+          break;
+        case "neato":
+          resultGraph = Viz(graphDot, { format: "svg", engine: "neato" });
+          break;
+        case "osage":
+          resultGraph = Viz(graphDot, { format: "svg", engine: "osage" });
+          break;
+        case "twopi":
+          resultGraph = Viz(graphDot, { format: "svg", engine: "twopi" });
+          break;
+        default:
+          resultGraph = Viz(graphDot, { format: "svg" });
+          break;
+      }
+      res.write(resultGraph);
+      res.end();
+    })
 }).listen(8001);
 
 function rankSep(data) {
   var rankMatkulWajibSemester = [];
   var rankMatkulPilihanSemester = [];
   for (var i = 0; i < data.length; i++) {
-      if(data[i].wajib){
-          if(!rankMatkulWajibSemester.hasOwnProperty(data[i].semester)){
-            rankMatkulWajibSemester[data[i].semester] = [];
-          }
-          rankMatkulWajibSemester[data[i].semester].push("\"" + data[i].kode + "\"\;");
-       } else if(!data[i].wajib) {
-         if(!rankMatkulPilihanSemester.hasOwnProperty(data[i].semester)) {
-             rankMatkulPilihanSemester[data[i].semester] = [];
-         }
-         rankMatkulPilihanSemester[data[i].semester].push("\"" + data[i].kode + "\"\;");
+    if (data[i].wajib) {
+      if (!rankMatkulWajibSemester.hasOwnProperty(data[i].semester)) {
+        rankMatkulWajibSemester[data[i].semester] = [];
+      }
+      rankMatkulWajibSemester[data[i].semester].push("\"" + data[i].kode + "\"\;");
+    } else if (!data[i].wajib) {
+      if (pilihan === "true") {
+        if (!rankMatkulPilihanSemester.hasOwnProperty(data[i].semester)) {
+          rankMatkulPilihanSemester[data[i].semester] = [];
+        }
+        rankMatkulPilihanSemester[data[i].semester].push("\"" + data[i].kode + "\"\;");
+      } else {
+
+      }
     }
   }
 
@@ -46,90 +83,99 @@ function rankSep(data) {
     "\n",
     "node [shape=box];"
   ];
-  rankMatkulWajibSemester.forEach(function(item, index){
+  rankMatkulWajibSemester.forEach(function (item, index) {
     var str = '{rank = same; "Semester ' + index + '";' + item.join("") + '}';
     result.push(str);
   });
 
-   result.push("node [shape=box, color=green];");
-   rankMatkulPilihanSemester.forEach(function(item, index){
-     var str = '{rank = same; "Semester ' + index + '";' + item.join("") + '}';
-     result.push(str);
-   });
-
-   result.push("\n");
-   return result.join("\n");
+  if (pilihan === "true") {
+    result.push("node [shape=box, color=green];");
+    rankMatkulPilihanSemester.forEach(function (item, index) {
+      var str = '{rank = same; "Semester ' + index + '";' + item.join("") + '}';
+      result.push(str);
+    });
+  }
+  result.push("\n");
+  return result.join("\n");
 }
 
 function nodesMatkul(data) {
   var nodeMatkulSemester = [];
-    for (var i = 0; i < data.length; i++) {
-        if (data[i].wajib){
-            var str = "\"" + data[i].kode + "\"" + '[label=' + "\"" + data[i].kode + "-0" + data[i].sks + "\n" + data[i].nama + "\"" + ']';
-            if(!nodeMatkulSemester.hasOwnProperty(data[i].semester)){
-                nodeMatkulSemester[data[i].semester] = [];
-            }
-                nodeMatkulSemester[data[i].semester].push(str);
-        } 
+  //WAJIB
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].wajib) {
+      var str = "\"" + data[i].kode + "\"" + '[label=' + "\"" + data[i].kode + "-0" + data[i].sks + "\n" + data[i].nama + "\"" + ']';
+      if (!nodeMatkulSemester.hasOwnProperty(data[i].semester)) {
+        nodeMatkulSemester[data[i].semester] = [];
+      }
+      nodeMatkulSemester[data[i].semester].push(str);
     }
+  }
+  //PILIHAN
+  if (pilihan === "true") {
+    for (var i = 0; i < data.length; i++) {
+      if (!data[i].wajib) {
+        var str = "\"" + data[i].kode + "\"" + '[label=' + "\"" + data[i].kode + "-0" + data[i].sks + "\n" + data[i].nama + "\"" + ']';
+        if (!nodeMatkulSemester.hasOwnProperty(data[i].semester)) {
+          nodeMatkulSemester[data[i].semester] = [];
+        }
+        nodeMatkulSemester[data[i].semester].push(str);
+      }
+    }
+  }
+  
+  var node = [];
+  nodeMatkulSemester.forEach(function (item) {
+    node.push(item.join(""));
+  });
 
-     for (var i = 0; i < data.length; i++) {
-         if (!data[i].wajib){
-             var str = "\"" + data[i].kode + "\"" + '[label=' + "\"" + data[i].kode + "-0" + data[i].sks + "\n" + data[i].nama + "\"" + ']';
-             if(!nodeMatkulSemester.hasOwnProperty(data[i].semester)){
-                 nodeMatkulSemester[data[i].semester] = [];
-             }
-                 nodeMatkulSemester[data[i].semester].push(str);
-         } 
-     }
-    var node = [];
-    nodeMatkulSemester.forEach(function(item){
-        node.push(item.join(""));
-    });
-
-    return node.join("\n");
+  return node.join("\n");
 }
 
-function edgesMatkul (data) {
+function edgesMatkul(data) {
   var edgeMatkul = [];
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].wajib) {
+      for (var j = 0; j < data[i].prasyarat.lulus.length; j++) {
+        var str = "\"" + data[i].prasyarat.lulus[j] + "\" -> \"" + data[i].kode + "\"";
+        edgeMatkul.push(str);
+      }
+    }
+  }
+
+  if (pilihan === "true") {
+    for (var i = 0; i < data.length; i++) {
+      if (!data[i].wajib) {
+        for (var j = 0; j < data[i].prasyarat.lulus.length; j++) {
+          var str = "\"" + data[i].prasyarat.lulus[j] + "\" -> \"" + data[i].kode + "\"";
+          edgeMatkul.push(str);
+        }
+      }
+    }
+  }
   
-    for (var i = 0; i < data.length; i++) {
-        if(data[i].wajib){
-            for (var j = 0; j < data[i].prasyarat.lulus.length; j++) {
-              var str = "\"" + data[i].prasyarat.lulus[j] + "\" -> \"" + data[i].kode + "\"";
-              edgeMatkul.push(str);
-            }
-        }
+  edgeMatkul.push("\n");
+  edgeMatkul.push("edge [style=dotted];");
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].wajib) {
+      for (var j = 0; j < data[i].prasyarat.tempuh.length; j++) {
+        var str = "\"" + data[i].prasyarat.tempuh[j] + "\" -> \"" + data[i].kode + "\"";
+        edgeMatkul.push(str);
+      }
     }
+  }
 
-     for (var i = 0; i < data.length; i++) {
-         if(!data[i].wajib){
-             for (var j = 0; j < data[i].prasyarat.lulus.length; j++) {
-                 var str = "\"" + data[i].prasyarat.lulus[j] + "\" -> \"" + data[i].kode + "\"";
-                 edgeMatkul.push(str);
-             }
-         }
-     }
-
-    edgeMatkul.push("\n");
-    edgeMatkul.push("edge [style=dotted];");
-
+  if(pilihan === "true") {
     for (var i = 0; i < data.length; i++) {
-        if(data[i].wajib){
-            for (var j = 0; j < data[i].prasyarat.tempuh.length; j++) {
-                var str = "\"" + data[i].prasyarat.tempuh[j] + "\" -> \"" + data[i].kode + "\"";
-                edgeMatkul.push(str);
-            }
+      if (!data[i].wajib) {
+        for (var j = 0; j < data[i].prasyarat.tempuh.length; j++) {
+          var str = "\"" + data[i].prasyarat.tempuh[j] + "\" -> \"" + data[i].kode + "\"";
+          edgeMatkul.push(str);
         }
+      }
     }
-
-     for (var i = 0; i < data.length; i++) {
-         if(!data[i].wajib){
-             for (var j = 0; j < data[i].prasyarat.tempuh.length; j++) {
-                 var str = "\"" + data[i].prasyarat.tempuh[j] + "\" -> \"" + data[i].kode + "\"";
-                 edgeMatkul.push(str);
-             }
-         }
-     }
-    return edgeMatkul.join("");
+  }
+  return edgeMatkul.join("");
 }
